618b003130ed6c59287a501f8cdb8660d144fd61
cg4g19 <cg4g19@soton.ac.uk>
2020-02-25 18:29:09 +0000

    Updated countreviews to sort the temporary txt file by amount of reviews and output

--- countreviews.sh
+++ countreviews.sh
@@ -4,4 +4,5 @@ do
 filename=$(echo $hotel | sed -e 's/.*\/hotel/hotel/')
 grep -E 'Author|Content' $hotel | wc -l | awk '{print $0/2}' | awk -v var="$filename" '{print var, $0}' >> tmp.txt
 done
+sort -nrk 2,2 tmp.txt
 rm tmp.txt
