3215197b1d58f9c0a7774e38971b0b65bccd4576
cg4g19 <cg4g19@soton.ac.uk>
2020-02-25 18:27:16 +0000

    Fixed countreviews temporary txt file was created and removed for each file rather than for the collective

--- countreviews.sh
+++ countreviews.sh
@@ -3,5 +3,5 @@ for hotel in $@/*
 do
 filename=$(echo $hotel | sed -e 's/.*\/hotel/hotel/')
 grep -E 'Author|Content' $hotel | wc -l | awk '{print $0/2}' | awk -v var="$filename" '{print var, $0}' >> tmp.txt
-rm tmp.txt
 done
+rm tmp.txt
