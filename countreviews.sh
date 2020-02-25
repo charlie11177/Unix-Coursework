bbdd85f1eced71e8181638d7f45eee275c199af3
cg4g19 <cg4g19@soton.ac.uk>
2020-02-25 17:11:54 +0000

    Updated countreviews to output on one line

--- countreviews.sh
+++ countreviews.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for hotel in $@/*
 do
-grep -E 'Author|Content' $hotel | wc -l | awk '{print $0/2}'
-echo $hotel | sed -e 's/.*\/hotel/hotel/'
+filename=$(echo $hotel | sed -e 's/.*\/hotel/hotel/')
+grep -E 'Author|Content' $hotel | wc -l | awk '{print $0/2}' | awk -v var="$filename" '{print var, $0}'
 done
