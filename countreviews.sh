6bd6858b6ee2ace9062c2f1f5c948672f02888b9
cg4g19 <cg4g19@soton.ac.uk>
2020-02-25 17:05:41 +0000

    Updated countreviews to output the filename alongside the amount of reviews

--- countreviews.sh
+++ countreviews.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 for hotel in $@/*
 do
-	grep -E 'Author|Content' $hotel | wc -l | awk '{print $0/2}'
+grep -E 'Author|Content' $hotel | wc -l | awk '{print $0/2}'
+echo $hotel | sed -e 's/.*\/hotel/hotel/'
 done
