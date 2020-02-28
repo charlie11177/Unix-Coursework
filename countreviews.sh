4ae447efed7903211f86aab59a1abaa637c8be6a
cg4g19 <cg4g19@soton.ac.uk>
2020-02-28 13:22:10 +0000

    Updated countreviews to omit .dat from the filename

--- countreviews.sh
+++ countreviews.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for hotel in $@/*
 do
-filename=$(echo $hotel | sed -e 's/.*\/hotel/hotel/')
+filename=$(echo $hotel | sed -e 's/.*\/hotel/hotel/' | sed 's/.dat//')
 grep -E 'Author|Content' $hotel | wc -l | awk '{print $0/2}' | awk -v var="$filename" '{print var, $0}'
 done | sort -nrk 2,2
